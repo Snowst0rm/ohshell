72537e999573dc64276b54f3dd2b9ff04999dd7a
SnowstormNet <Snowst0rm@outlook.com>
2019-02-16 02:06:14 +0800

    Create Msf.sh

---
+++ Msf.sh
@@ -0,0 +1,23 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+pkg install python
+
+pkg install python2
+
+pkg install curl
+
+pkg install wget
+
+pkg install perl
+
+pkg install ruby
+
+pkg install php
+
+pkg install toilet
+
+toilet download
+
+toilet Metasploit
+
+wget https://Auxilus.github.io/metasploit.sh
